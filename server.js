9ec8e41c98d7a477107a3b11f84495c3f6b27ced
Muhammedrasool <muhammedrasool04@gmail.com>
2025-11-20 13:54:15 +0530

    cors error

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(cors({
     origin:["http://localhost:5173","http://localhost:5174"] ,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type"]
   }));
+
+  app.options("*",cors())
   
 // Connect DB
 connectDB();
