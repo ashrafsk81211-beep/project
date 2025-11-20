39b7061f59315affa98355a4773c0b0b8402e633
Muhammedrasool <muhammedrasool04@gmail.com>
2025-11-20 13:45:55 +0530

    cors resolved

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ app.use(express.json());
 
 app.use(linkRouter)
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    allowedHeaders: ["Content-Type"]
   }));
   
-app.use("*",cors())
 // Connect DB
 connectDB();
 
